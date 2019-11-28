fc2857fe239346083acc07f0fd7bd30787727f4b
thomasbitacademy <57895636+thomasbitacademy@users.noreply.github.com>
2019-11-28 13:30:25 +0100

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bit Academy</title>
+    </head>
+
+    <body>
+        <h1>Skateboard <small>(#1)</small></h1>
+        <h1>Basketbal <small>(#2)</small></h1>
+        <h1>Skeelers <small>(#3)</small></h1>
+
+        <!-- maak hier de opdracht -->
+	<?php
+		echo "hi";
+	?>
+    </body>
+</html>
